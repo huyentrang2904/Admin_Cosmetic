9096b2db3a1fdc327f2c4ac54a45f00da93efb49
huyentrang2904 <tongtranga@gmail.com>
2024-07-18 17:45:37 +0700

    fix permission

--- admin/components/Sidebar/Sidebar.js
+++ admin/components/Sidebar/Sidebar.js
@@ -7,7 +7,7 @@ import { ChartPieIcon, BanknotesIcon, ReceiptPercentIcon, UsersIcon, ShoppingCar
 import { toast } from "react-toastify";
 
 const adminNavigation = [
-  { name: "Dashboard", href: "/dashboard", icon: ChartPieIcon, current: false },
+  { name: "Dashboard", href: "/dashboard", requiredPermissions: ["Manage products", "Manage permissions", "Manage discounts ", "Manage orders"], icon: ChartPieIcon, current: false },
   {
     name: "Product Management", href: "", requiredPermissions: "Manage products", icon: BanknotesIcon, current: false, children: [
       { name: 'Product List', href: '/products', current: false },
@@ -23,18 +23,20 @@ const adminNavigation = [
   },
   { name: "Discount Management", href: "/coupcons", requiredPermissions: "Manage discounts", icon: ReceiptPercentIcon, current: false },
   { name: "Order Management", href: "/orders", requiredPermissions: "Manage orders", icon: ShoppingCartIcon, current: false },
-
 ];
 
 export default function Sidebar() {
   const [auth, setAuth] = useState();
-  let value;
+  const [Role, setRole] = useState();
+  let value, value1;
   if (typeof window !== "undefined") {
     value = JSON.parse(localStorage.getItem("userInformation")) || null;
+    value1 = JSON.parse(localStorage.getItem("UserRole")) || null;
   }
   useEffect(() => {
     // Get the value from local storage if it exists
     setAuth(value.user);
+    setRole(value1.permissions)
   }, []);
   function redirect() {
     window.location.href = '/'
@@ -42,9 +44,21 @@ export default function Sidebar() {
   function handleLogout() {
     setAuth("");
     localStorage.clear();
-    toast.success("Bạn đã đăng xuất");
+    toast.success("Log out successfully");
     setTimeout(redirect, 1000)
   }
+  const matchingItems = adminNavigation.filter(navItem => {
+    // Kiểm tra nếu requiredPermissions là một mảng, nếu không, chuyển đổi thành mảng gồm một phần tử
+    const requiredPerms = Array.isArray(navItem.requiredPermissions) ? navItem.requiredPermissions : [navItem.requiredPermissions];
+    // Kiểm tra xem mỗi phần tử trong requiredPerms có trong permission
+    return requiredPerms.every(reqPerm =>
+      Role?.some(permItem =>
+        permItem.permissionName === reqPerm
+      )
+    );
+  });
+  matchingItems.unshift({ name: "Dashboard", href: "/dashboard", icon: ChartPieIcon, current: false });
+  console.log(matchingItems)
   return (
     <div className="flex flex-row w-1/6 h-full">
       <div className="flex flex-col flex-1 h-screen min-h-0 bg-gray-800">
@@ -57,7 +71,7 @@ export default function Sidebar() {
             aria-label="Sidebar"
           >
             <For
-              each={adminNavigation}
+              each={matchingItems}
               render={(item, index) => (
                 <>
                   <If isTrue={!item.children}>
