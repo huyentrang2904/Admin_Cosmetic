243e8e278dcfbd9ffe076e8654e51ac87b19d6f9
Abilene-may <nguyenthinga270702@gmail.com>
2024-07-18 16:36:23 +0700

    mapping api for dashboard

--- admin/components/DashBoard/DashboardComponent.js
+++ admin/components/DashBoard/DashboardComponent.js
@@ -6,62 +6,92 @@ import { getDataAnalytic } from '@/state/Admin/Action';
 
 ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend, ArcElement);
 
+export function formatCurrency(num, to = 0, currency = "USD") {
+  let newNum = Number.parseFloat(num).toFixed(to);
+  switch (currency) {
+    case "USD":
+      return `$${Number(newNum).toLocaleString('en-US')}`;
+    default:
+      return `${Number(newNum).toLocaleString('vi-VN')} VND`;
+  }
+}
+
+
 const DashboardComponent = () => {
   const dispatch = useDispatch()
-  const test = useSelector(store => store?.admin?.analytic)
-  console.log(test)
+  const dataAnalytic = useSelector(store => store?.admin?.analytic)
+  console.log(dataAnalytic)
   useEffect(() => {
     dispatch(getDataAnalytic());
   }, []);
+
   const barData = {
-    labels: ['1', '2', '3', '4', '5', '6', '7'],
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
     datasets: [
       {
         label: 'Revenue of 2024',
-        data: [12, 19, 3, 5, 2, 3, 7],
+        data: [
+          dataAnalytic?.statisticalDTO?.totalRevenueOfJanuary || 0,
+          dataAnalytic?.statisticalDTO?.totalRevenueOfFebruary || 0,
+          dataAnalytic?.statisticalDTO?.totalRevenueOfMarch || 0,
+          dataAnalytic?.statisticalDTO?.totalRevenueOfApril || 0,
+          dataAnalytic?.statisticalDTO?.totalRevenueOfMay || 0,
+          dataAnalytic?.statisticalDTO?.totalRevenueOfJune || 0,
+          dataAnalytic?.statisticalDTO?.totalRevenueOfJuly || 0
+        ],
         backgroundColor: 'rgba(75, 192, 192, 0.6)',
       },
     ],
   };
 
   const pieData = {
-    labels: ['Brand A', 'Brand B', 'Brand C'],
+    labels: dataAnalytic?.revenueOfBrandDTO?.map(brand => brand.brandName) || [],
     datasets: [
       {
-        data: [300, 50, 100],
+        data: dataAnalytic?.revenueOfBrandDTO?.map(brand => brand.totalRevenueOfYear) || [],
         backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
       },
     ],
   };
-  if (test) return (
+
+  return (
     <div className="h-full p-8 overflow-y-scroll bg-white">
       <div className="grid grid-cols-2 gap-4">
         <div className="col-span-4 text-center">
           <h1 className="text-2xl font-bold">Dashboard</h1>
         </div>
-        <div className='grid grid-cols-5 col-span-4 gap-4'>
+        <div className="grid grid-cols-5 col-span-4 gap-4">
           <div className="col-span-1 p-4 border border-gray-200 rounded">
             <h2 className="text-xl text-center">Total orders</h2>
-            <div className="text-xl font-semibold text-center">{test?.statisticalDTO.totalOrders}</div>
+            <div className="text-xl font-semibold text-center">
+              {dataAnalytic?.statisticalDTO?.totalOrders}
+            </div>
           </div>
           <div className="col-span-1 p-4 border border-gray-200 rounded">
             <h2 className="text-xl text-center">Canceled</h2>
-            <div className="text-xl font-semibold text-center">{test?.statisticalDTO.canceledOrders}</div>
+            <div className="text-xl font-semibold text-center">
+              {dataAnalytic?.statisticalDTO?.canceledOrders}
+            </div>
           </div>
           <div className="col-span-1 p-4 border border-gray-200 rounded">
             <h2 className="text-xl text-center">Order successful</h2>
-            <div className="text-xl font-semibold text-center">{test?.statisticalDTO.orderSuccessful}</div>
+            <div className="text-xl font-semibold text-center">
+              {dataAnalytic?.statisticalDTO?.orderSuccessful}
+            </div>
           </div>
           <div className="col-span-1 p-4 border border-gray-200 rounded">
             <h2 className="text-xl text-center">Pending Orders</h2>
-            <div className="text-xl font-semibold text-center">{test?.statisticalDTO.pendingOrders}</div>
+            <div className="text-xl font-semibold text-center">
+              {dataAnalytic?.statisticalDTO?.pendingOrders}
+            </div>
           </div>
           <div className="col-span-1 p-4 border border-gray-200 rounded">
             <h2 className="text-xl text-center">Total revenue of July</h2>
-            <div className="text-xl font-semibold text-center">{test?.statisticalDTO.totalRevenueOfJuly}</div>
+            <div className="text-xl font-semibold text-center">
+              {formatCurrency(dataAnalytic?.statisticalDTO?.totalRevenueOfJuly)}
+            </div>
           </div>
         </div>
-
 
         <div className="col-span-2">
           <Bar data={barData} />
@@ -83,17 +113,18 @@ const DashboardComponent = () => {
               </tr>
             </thead>
             <tbody>
-              {/* Add rows here */}
-              <tr>
-                <td className="p-2 border">Product 1</td>
-                <td className="p-2 border">10</td>
-                <td className="p-2 border">5</td>
-              </tr>
-              <tr>
-                <td className="p-2 border">Product 2</td>
-                <td className="p-2 border">0</td>
-                <td className="p-2 border">3</td>
-              </tr>
+              {dataAnalytic?.productStatisticDTOS?.map((item, index) => (
+                <tr key={index}>
+                  <td className="p-2 border ">
+                    <div title={item.title} className="w-96 text-nowrap overflow-x-hidden  text-ellipsis">
+                      {item.title}
+                    </div>
+                  </td>{" "}
+                  {/* w-64 là 16rem, bạn có thể điều chỉnh giá trị này */}
+                  <td className="p-2 border">{item.quantity}</td>
+                  <td className="p-2 border">{item.countPurchase}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
@@ -108,14 +139,15 @@ const DashboardComponent = () => {
               </tr>
             </thead>
             <tbody>
-              {test?.userPotentialDTOS.map((data, index) => (
+              {dataAnalytic?.userPotentialDTOS?.map((data, index) => (
                 <tr key={index}>
                   <td className="p-2 border">{data.username}</td>
                   <td className="p-2 border">{data.totalOrders}</td>
-                  <td className="p-2 border">{data.totalSpend}$</td>
+                  <td className="p-2 border">
+                    {formatCurrency(data.totalSpend)}
+                  </td>
                 </tr>
               ))}
-              {/* Add rows here */}
             </tbody>
           </table>
         </div>
